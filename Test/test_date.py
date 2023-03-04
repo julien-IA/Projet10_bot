2fa2732d70072394de89f039f6b3d41f331211ee
julien <12434976+julien-IA@users.noreply.github.com>
2023-03-04 22:36:32 +0100

    ajustement requirements

--- Test/test_date.py
+++ Test/test_date.py
@@ -1,21 +1,21 @@
-from dialogs import booking_dialog as bd
+from dialogs.booking_dialog import BookingDialog
 import unittest
 
 class Test_TestDateValidator(unittest.TestCase):
 
     def test_is_depart_before_return(self):
-        test_BookingDialog = bd.BookingDialog()
+        test_BookingDialog = BookingDialog()
         self.assertTrue(test_BookingDialog.is_depart_before_return('2023-01-15','2023-01-29'))
         self.assertFalse(test_BookingDialog.is_depart_before_return('2023-01-29','2023-01-15'))
 
     def test_is_ambiguous(self):
-        test_BookingDialog = bd.BookingDialog()
+        test_BookingDialog = BookingDialog()
         self.assertTrue(test_BookingDialog.is_ambiguous('not_a_date'))
         self.assertTrue(test_BookingDialog.is_ambiguous('123546798'))
         self.assertFalse(test_BookingDialog.is_ambiguous('2023-02-15'))
 
     def test_is_valid_date(self):
-        test_BookingDialog = bd.BookingDialog()
+        test_BookingDialog = BookingDialog()
         self.assertTrue(test_BookingDialog.is_valid_date('2023-01-01'))
         self.assertFalse(test_BookingDialog.is_ambiguous('2023-02-32'))
 
