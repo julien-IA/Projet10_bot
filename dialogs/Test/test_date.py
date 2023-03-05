e4b25d7a502c02fd3c7e475cace9ca40a4f122f8
julien <12434976+julien-IA@users.noreply.github.com>
2023-03-05 23:55:30 +0100

    Déplacmement des tests

--- dialogs/Test/test_date.py
+++ dialogs/Test/test_date.py
@@ -13,9 +13,7 @@ class Test_TestDateValidator(unittest.TestCase):
         test_BookingDialog = BookingDialog()
         self.assertTrue(test_BookingDialog.is_ambiguous('not_a_date'))
         self.assertTrue(test_BookingDialog.is_ambiguous('123546798'))
-        self.assertFalse(test_BookingDialog.is_ambiguous('2023-02-15'))
-        self.assertTrue(test_BookingDialog.is_ambiguous('2023-01-01'))
-        self.assertFalse(test_BookingDialog.is_ambiguous('2023-02-32'))      
+        self.assertFalse(test_BookingDialog.is_ambiguous('2023-02-15'))    
 
 if __name__ == '__main__':
     unittest.main()
