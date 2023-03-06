3d8837bb694558a613808211f473b70a4f6ab01c
julien <12434976+julien-IA@users.noreply.github.com>
2023-03-06 07:51:23 +0100

    ajustement luis_helper

--- helpers/luis_helper.py
+++ helpers/luis_helper.py
@@ -157,13 +157,13 @@ class LuisHelper:
         except Exception as exception:
             print(exception)
 
-        intent_json=json.dumps(recognizer_result.intents, default=lambda x: x.to_json())
-        entities_json=json.dumps(recognizer_result.entities)
-        print("intent = ", intent_json)        
-        print("result = ", entities_json)
-
-        utterance_log = UtteranceLog()
         try:
+            intent_json=json.dumps(recognizer_result.intents, default=lambda x: x.to_json())
+            entities_json=json.dumps(recognizer_result.entities)
+            print("intent = ", intent_json)        
+            print("result = ", entities_json)
+
+            utterance_log = UtteranceLog()
             utterance_log.set_utterance_values(intent=intent_json, entities=entities_json)
         except:
             pass
